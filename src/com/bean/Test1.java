33236c9318e4f59cc3c600dca95e8e58c15ff0f2
Lenovo <znrsg0223@163.com>
2022-10-18 09:59:45 +0800

    版本三

--- src/com/bean/Test1.java
+++ src/com/bean/Test1.java
@@ -3,5 +3,6 @@ package com.bean;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaavvvvvvvv");
+        System.out.println("mmmmmmmmmmm");
     }
 }
